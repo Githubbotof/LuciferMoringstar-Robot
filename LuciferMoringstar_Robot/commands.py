31c11c264fb333a1ccb2a65ec1ef21e242d244fd
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-23 22:05:25 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -82,8 +82,9 @@ async def start(bot: lucifermoringstar_robot, update):
     if len(update.command) != 2:
         pr0fess0r_99 = [[ InlineKeyboardButton("× 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ×", url=f"http://t.me/{temp.Bot_Username}?startgroup=true") ],
                       [ InlineKeyboardButton("🤖 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🤖", url=f"t.me/supprotosfmebot"), InlineKeyboardButton("🔥 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 🔥", url="t.me/linkzofallmygroups") ],
-                      [ InlineKeyboardButton("🔅 𝙷𝙴𝙻𝙿 🔅", callback_data="help"), InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃 🤠", callback_data="about") ]] 
-        await bot.send_photo(photo=random.choice(PICS), chat_id=update.chat.id, caption=START_MESSAGE.format(mention=update.from_user.mention, name=temp.Bot_Name, username=temp.Bot_Username), reply_markup=InlineKeyboardMarkup(pr0fess0r_99))
+                      [ InlineKeyboardButton("🔅 𝙷𝙴𝙻𝙿 🔅", callback_data="help"), InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃 🤠", callback_data="about") ]]
+        await bot.send_sticker(sticker=random.choice(PICS)
+        await bot.send_message(chat_id=update.chat.id, text=START_MESSAGE.format(mention=update.from_user.mention, name=temp.Bot_Name, username=temp.Bot_Username), reply_markup=InlineKeyboardMarkup(pr0fess0r_99))
         
 @lucifermoringstar_robot.on_message(filters.command(["admin", "admins"]) & filters.user(ADMINS) & filters.private, group=2)
 async def admin(bot: lucifermoringstar_robot, update):
