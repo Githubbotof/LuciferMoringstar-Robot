1bfb7a5977b4ef5df7b6a0f5081264da3431b349
Githubbotof <89619464+Githubbotof@users.noreply.github.com>
2022-07-24 19:32:11 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -26,7 +26,7 @@
 
 import random, asyncio
 from pyrogram import Client as lucifermoringstar_robot , filters, enums
-from LuciferMoringstar_Robot import temp, SUPPORT, PICS, ADMINS, CREATOR_USERNAME, CREATOR_NAME, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, SAVE_FILES, START_MESSAGE, STICKERS
+from LuciferMoringstar_Robot import temp, SUPPORT, PICS, ADMINS, CREATOR_USERNAME, CREATOR_NAME, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, SAVE_FILES, START_MESSAGE
 from LuciferMoringstar_Robot.translation import SETTINGS_MESSAGE, ADMIN_CMD_MESSAGE, ABOUT_MESSAGE, USAGE_MESSAGE
 from LuciferMoringstar_Robot.functions import get_settings, save_group_settings
 from LuciferMoringstar_Robot.admins.broadcast import send_broadcast
@@ -83,7 +83,7 @@ async def start(bot: lucifermoringstar_robot, update):
         pr0fess0r_99 = [[ InlineKeyboardButton("× 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ×", url=f"http://t.me/{temp.Bot_Username}?startgroup=true") ],
                       [ InlineKeyboardButton("🤖 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🤖", url=f"t.me/supprotosfmebot"), InlineKeyboardButton("🔥 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 🔥", url="t.me/linkzofallmygroups") ],
                       [ InlineKeyboardButton("🔅 𝙷𝙴𝙻𝙿 🔅", callback_data="help"), InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃 🤠", callback_data="about") ]]
-        await bot.send_sticker(chat_id=update.chat.id, sticker=random.choice(STICKERS))  #random.choice(STICKERS)
+        await bot.send_sticker(chat_id=update.chat.id, sticker="CAACAgUAAxkBAAIBRWLcGqkQY6eq3FdOnmDy03v6S8pgAAJZBwACWEDhVjuRKDCnf4OTHgQ")  #random.choice(PICS)
         await bot.send_message(chat_id=update.chat.id, text=START_MESSAGE.format(mention=update.from_user.mention, name=temp.Bot_Name, username=temp.Bot_Username), reply_markup=InlineKeyboardMarkup(pr0fess0r_99), disable_web_page_preview=True)
         
 @lucifermoringstar_robot.on_message(filters.command(["admin", "admins"]) & filters.user(ADMINS) & filters.private, group=2)
